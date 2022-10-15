37c1e37ba4ac5c095143f348b24364cdcaf13458
Jordan Yefet <Jordan.Yefet90@gmail.com>
2022-10-15 20:37:47 +0200

    added 2 buttons to settings page and made it decupled from main parameters

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import './App.css';
+import "./App.css";
 import Timer from "./Timer";
 import Settings from "./Settings";
-import {useState} from "react";
+import { useState } from "react";
 import SettingsContext from "./SettingsContext";
 
 function App() {
+  const initialStates = {
+    workMinutes: 45,
+    breakMinutes: 15,
+    longBreakMinutes: 30,
+    breakIntervals: 4,
+    autoStart: true,
+  };
 
   const [showSettings, setShowSettings] = useState(false);
-  const [workMinutes, setWorkMinutes] = useState(45);
-  const [breakMinutes, setBreakMinutes] = useState(15);
+  const [workMinutes, setWorkMinutes] = useState(initialStates.workMinutes);
+  const [breakMinutes, setBreakMinutes] = useState(initialStates.breakMinutes);
+  const [longBreakMinutes, setLongBreakMinutes] = useState(
+    initialStates.longBreakMinutes
+  );
+  const [breakIntervals, setBreakIntervals] = useState(
+    initialStates.breakIntervals
+  );
+  const [autoStart, setAutoStart] = useState(initialStates.autoStart);
 
   return (
     <main>
-      <SettingsContext.Provider value={{
-        showSettings,
-        setShowSettings,
-        workMinutes,
-        breakMinutes,
-        setWorkMinutes,
-        setBreakMinutes,
-      }}>
+      <SettingsContext.Provider
+        value={{
+          showSettings,
+          setShowSettings,
+          workMinutes,
+          breakMinutes,
+          setWorkMinutes,
+          setBreakMinutes,
+          longBreakMinutes,
+          setLongBreakMinutes,
+          breakIntervals,
+          setBreakIntervals,
+          initialStates,
+          autoStart,
+          setAutoStart,
+        }}
+      >
         {showSettings ? <Settings /> : <Timer />}
       </SettingsContext.Provider>
     </main>
--- src/Settings.js
+++ src/Settings.js
@@ -1,38 +1,104 @@
-import ReactSlider from 'react-slider';
-import './slider.css'
+import ReactSlider from "react-slider";
+import "./slider.css";
 import SettingsContext from "./SettingsContext";
-import {useContext} from "react";
+import { useContext, useState } from "react";
 import BackButton from "./BackButton";
-
 
 function Settings() {
   const settingsInfo = useContext(SettingsContext);
-  return(
-    <div style={{textAlign:'left'}}>
-      <label>work: {settingsInfo.workMinutes}:00</label>
-      <ReactSlider
-        className={'slider'}
-        thumbClassName={'thumb'}
-        trackClassName={'track'}
-        value={settingsInfo.workMinutes}
-        onChange={newValue => settingsInfo.setWorkMinutes(newValue)}
-        min={1}
-        max={120}
-      />
-      <label>break: {settingsInfo.breakMinutes}:00</label>
-      <ReactSlider
-        className={'slider green'}
-        thumbClassName={'thumb'}
-        trackClassName={'track'}
-        value={settingsInfo.breakMinutes}
-        onChange={newValue => settingsInfo.setBreakMinutes(newValue)}
-        min={1}
-        max={120}
-      />
-      <div style={{textAlign:'center', marginTop:'20px'}}>
+  //const [apply, setApply] = useState(false);
+
+  const [tempWorkMinutes, setTempWorkMinutes] = useState(
+    settingsInfo.workMinutes
+  );
+  const [tempBreakMinutes, setTempBreakMinutes] = useState(
+    settingsInfo.breakMinutes
+  );
+  const [tempLongBreakMinutes, setTempLongBreakMinutes] = useState(
+    settingsInfo.longBreakMinutes
+  );
+  const [tempBreakIntervals, setTempBreakIntervals] = useState(
+    settingsInfo.breakIntervals
+  );
+  const [tempAutoStart, setTempAutoStart] = useState(settingsInfo.autoStart);
+
+  function resetBtn() {
+    setTempWorkMinutes(settingsInfo.initialStates.workMinutes);
+    setTempBreakMinutes(settingsInfo.initialStates.breakMinutes);
+    setTempLongBreakMinutes(settingsInfo.initialStates.longBreakMinutes);
+    setTempBreakIntervals(settingsInfo.initialStates.breakIntervals);
+    setTempAutoStart(settingsInfo.initialStates.autoStart);
+  }
+
+  function applyBtn() {
+    settingsInfo.setWorkMinutes(tempWorkMinutes);
+    settingsInfo.setBreakMinutes(tempBreakMinutes);
+    settingsInfo.setLongBreakMinutes(tempLongBreakMinutes);
+    settingsInfo.setBreakIntervals(tempBreakIntervals);
+    settingsInfo.setAutoStart(tempAutoStart);
+  }
+
+  return (
+    <div style={{ textAlign: "left" }}>
+      <div>
         <BackButton onClick={() => settingsInfo.setShowSettings(false)} />
       </div>
-
+      <label>work: {tempWorkMinutes}:00</label>
+      <ReactSlider
+        className={"slider"}
+        thumbClassName={"thumb"}
+        trackClassName={"track"}
+        value={tempWorkMinutes}
+        onChange={(newValue) => setTempWorkMinutes(newValue)}
+        min={1}
+        max={120}
+      />
+      <label>break: {tempBreakMinutes}:00</label>
+      <ReactSlider
+        className={"slider green"}
+        thumbClassName={"thumb"}
+        trackClassName={"track"}
+        value={tempBreakMinutes}
+        onChange={(newValue) => setTempBreakMinutes(newValue)}
+        min={1}
+        max={120}
+      />
+      <label>long break: {tempLongBreakMinutes}:00</label>
+      <ReactSlider
+        className={"slider blue"}
+        thumbClassName={"thumb"}
+        trackClassName={"track"}
+        value={tempLongBreakMinutes}
+        onChange={(newValue) => setTempLongBreakMinutes(newValue)}
+        min={1}
+        max={120}
+      />
+      <label>break intervals: {tempBreakIntervals}</label>
+      <ReactSlider
+        className={"slider yellow"}
+        thumbClassName={"thumb"}
+        trackClassName={"track"}
+        value={tempBreakIntervals}
+        onChange={(newValue) => setTempBreakIntervals(newValue)}
+        min={1}
+        max={5}
+      />
+      <div className="flex auto-start">
+        <label>Auto break start?</label>
+        <input
+          type="checkbox"
+          checked={tempAutoStart}
+          onChange={() => setTempAutoStart(!tempAutoStart)}
+        />
+      </div>
+      <div className="flex settings-btn-container">
+        <button className="btn-with-text" onClick={resetBtn}>
+          Reset
+        </button>
+        <button className="btn-with-text" onClick={applyBtn()}>
+          Apply
+        </button>
+      </div>
     </div>
   );
 }
