25791ae211a2e50dc45a6d3a90f15c5af5db04ba
Jordan Yefet <Jordan.Yefet90@gmail.com>
2022-11-05 01:23:06 +0200

    finished restart button

--- src/Timer.js
+++ src/Timer.js
@@ -9,24 +9,35 @@ import { useContext, useState, useEffect, useRef } from "react";
 import SettingsContext from "./SettingsContext";
 import { Link } from "react-router-dom";
 
-const red = "#f54e4e";
-const green = "#4aec8c";
-const blue = "#40a3ff";
+const colors = {
+  red: "#f54e4e",
+  green: "#4aec8c",
+  blue: "#40a3ff",
+};
 
 function Timer() {
   const settingsInfo = useContext(SettingsContext);
-  const [restart, setRestart] = useState(false);
-  const [isPaused, setIsPaused] = useState(true);
-  const [mode, setMode] = useState("work"); // work/break/null
-  const [secondsLeft, setSecondsLeft] = useState(0);
-  const [isCacheMem, setIsCacheMeme] = useState(false);
-  const [breakIntervalsDone, setBreakIntervalsDone] = useState(-1);
-  const breakIntervalsDoneRef = useRef(-1); // filled yellow indicators
+  const initialStates = {
+    restart: false,
+    isPaused: true,
+    mode: "work",
+    secondsLeft: settingsInfo.workMinutes * 60,
+    breakIntervalsDone: -1,
+  };
+  const [restart, setRestart] = useState(initialStates.restart);
+  const [isPaused, setIsPaused] = useState(initialStates.isPaused);
+  const [mode, setMode] = useState(initialStates.mode); // work/break/null
+  const [secondsLeft, setSecondsLeft] = useState(initialStates.secondsLeft);
+  //const [isCacheMem, setIsCacheMeme] = useState(false);
+  const [breakIntervalsDone, setBreakIntervalsDone] = useState(
+    initialStates.breakIntervalsDone
+  );
+  const breakIntervalsDoneRef = useRef(breakIntervalsDone); // filled yellow indicators
   const secondsLeftRef = useRef(secondsLeft);
   const isPausedRef = useRef(isPaused);
   const modeRef = useRef(mode);
 
-  /*   useEffect(() => {
+  /*     useEffect(() => {
     setIsPaused(JSON.parse(window.localStorage.getItem("isPaused")));
     setSecondsLeft(JSON.parse(window.localStorage.getItem("secondsLeft")));
     isPausedRef.current = isPaused;
@@ -42,6 +53,18 @@ function Timer() {
     secondsLeftRef.current--;
     setSecondsLeft(secondsLeftRef.current);
   }
+
+  useEffect(() => {
+    setIsPaused(initialStates.isPaused);
+    isPausedRef.current = isPaused;
+    setBreakIntervalsDone(initialStates.breakIntervalsDone);
+    breakIntervalsDoneRef.current = breakIntervalsDone;
+    setMode(initialStates.mode);
+    modeRef.current = mode;
+    setSecondsLeft(initialStates.secondsLeft);
+    secondsLeftRef.current = secondsLeft;
+    setRestart(false);
+  }, [restart]);
 
   useEffect(() => {
     function switchMode() {
@@ -84,16 +107,13 @@ function Timer() {
           setBreakIntervalsDone((prevState) => prevState + 1);
         }
         if (modeRef.current === "longBreak") {
-          setIsPaused(true);
-          isPausedRef.current = true;
-          breakIntervalsDoneRef.current = -1;
-          setBreakIntervalsDone(-1);
+          setRestart(true);
         }
         return switchMode();
       }
 
       tick();
-    }, 1000);
+    }, 100);
     return () => clearInterval(interval);
   }, [settingsInfo]);
 
@@ -131,7 +151,12 @@ function Timer() {
           text={minutes + ":" + seconds}
           styles={buildStyles({
             textColor: "#fff",
-            pathColor: mode === "work" ? red : mode === "break" ? green : blue,
+            pathColor:
+              mode === "work"
+                ? colors.red
+                : mode === "break"
+                ? colors.green
+                : colors.blue,
             tailColor: "rgba(255,255,255,.2)",
           })}
         />
